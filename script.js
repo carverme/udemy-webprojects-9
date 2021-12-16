01538e17f70a07a6f5ab0f72f95a9960476fea6e
Matt Carver <mattcarver.wd@gmail.com>
2021-12-16 14:04:40 -0800

    working an error with posting new items

--- script.js
+++ script.js
@@ -17,6 +17,32 @@ const dummyTransactions = [
 
 let transactions = dummyTransactions;
 
+//Add transaction
+function addTransaction(e) {
+    e.preventDefault();
+
+    if(text.value.trim() === '' || amount.value.trim() === '') {
+        alert('Please add text and amount');    
+    } else {
+        const transaction = {
+            id: generateID(),
+            text: text.value,
+            amount: +amount.value
+        }
+        transactions.push(transaction);
+        addTransactionDOM(transaction);
+        updateValues();
+
+        text.value = '';
+        amount.value = '';
+    }
+}
+
+//Generate random ID
+function generateID() {
+    return Math.floor(Math.random() * 100000000);
+}
+
 //Add transactions to DOM list
 function addTransactionDOM(transaction) {
     // Get the sign
@@ -29,17 +55,45 @@ function addTransactionDOM(transaction) {
 
     item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}
-    </span> <button class="delete-btn">X</button>
-    `;
+    </span> <button class="delete-btn" onclick="removeTransaction(${transaction.id})">X</button>`;
 
     list.appendChild(item);
+}
 
+
+//Update the balance, income and the expenses
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+    const expense = (
+        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1
+        ).toFixed(2);
+
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+
+}
+
+//Remove transaction by ID
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+
+    init();
 }
 
 //Init App
 function init() {
     list.innerHTML = '';
     transactions.forEach(addTransactionDOM);
+    updateValues();
 }
 
 init();
+
+form.addEventListener('submit', addTransaction);
